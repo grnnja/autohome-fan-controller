e1356eee26770144049b2de59d61ad31de183327
grnnja <grnnja@gmail.com>
2019-05-07 22:31:26 -0700

    initial commit

---
+++ env template.c
@@ -0,0 +1,8 @@
+// Rename this file to env.c and add information
+
+#include "env.h"
+
+const char* ssid = "wifi network name";
+const char* password = "password";
+const char* mqttServer = "server address";
+const int mqttPort = 0 ; // port number
